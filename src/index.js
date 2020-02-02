468ea42f8ec6bc0991d470b0aa22f45ebee0c1c0
marco pereira <viniciopq@hotmail.com>
2020-02-02 15:50:47 -0600

    set up webpack and EsLint

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+
+const Hi = () => {
+    
+    return (
+        <div>
+            Hi.
+        </div>
+    );
+}
+
+render(<Hi />, document.getElementById('root'))
